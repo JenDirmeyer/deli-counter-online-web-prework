1b75114c362003555d1d6c8eec6d8f944aec6b7f
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-03-30 23:06:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,5 +21,8 @@ def take_a_number(katz_deli,new_person)
 def now_serving(katz_deli)
   if katz_deli == 0 
     puts "There is nobody waiting to be served!"
+  else
+    puts kats_deli.first 
+  end  
 end
   
